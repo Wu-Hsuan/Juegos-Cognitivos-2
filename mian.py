22835ce7fa750350c71db0fee2cd5a7f6ef7e9f6
Yu-Hsuan <58472945+Wu-Hsuan@users.noreply.github.com>
2023-10-13 12:44:55 -0600

    Update mian.py

--- mian.py
+++ mian.py
@@ -2,9 +2,9 @@ import module as m
 
 modojuego=(input("Bienvenido a Batalla Naval: n\ Escoja el modo de juego: Automático (A) o Manual (M): "))
 if modojuego in "Aa":
-    coloca_barcos(min_nave, m, tamano)
+    m.coloca_barcos(min_nave, m, tamano)
 elif modojuego in "Mm":
-    coloca_barcosmanual(min_nave, m, tamano)
+    m.coloca_barcosmanual(min_nave, m, tamano)
 else:
     print("Escoga uno de los modos")
     modojuego= (input("Bienvenido a Batalla Naval: n\ Escoja el modo de juego: Automático (A) o Manual (M): ")) 
