24c36f7aafe98a13ea31c15388a5ba8d6b35deae
Yu-Hsuan <58472945+Wu-Hsuan@users.noreply.github.com>
2023-10-15 21:09:08 -0600

    Update mian.py
    Terminar modo manual

--- mian.py
+++ mian.py
@@ -2,7 +2,6 @@ import module as m
 
 # datos basicos para funcionar las funciones
 tamano = 8
-min_nave = int(tamano*tamano*0.55/1)
 m = creaMatriz(tamano)
 
 modojuego=(input("Bienvenido a Batalla Naval: n\ Escoja el modo de juego: Automático (A) o Manual (M): "))
@@ -12,8 +11,11 @@ if modojuego in "Aa":
     print("Tendrás 35 oportunidades de ataque para eliminar todas las naves en la pantalla.")
     print("El número restante de ataques se multiplica por diez y se convierte en tu puntuación final")
     print("Por lo tanto, cuanto más ataques te queden, más alta será tu puntuación. ¡Aprovecha la oportunidad para eliminar las naves!")
+    min_nave = int(tamano*tamano*0.55/1)
+    #Inicializar el tablero
     Tablero = m.coloca_barcos(min_nave, m, tamano)
     m.muestra_tablero(Tablero,tamano)
+    # Para accumular disparos
     borra = 0
     while True:
       if "X" in Tablero[0] or "X" in Tablero[1] or "X" in Tablero[2] or "X" in Tablero[3] or "X" in Tablero[4] or "X" in Tablero[5] or "X" in Tablero[6] or "X" in Tablero[7]:
@@ -24,8 +26,18 @@ if modojuego in "Aa":
         print("El juego ha terminado. Es hora de calcular la puntuación")
         break
 elif modojuego in "Mm":
-    tablero = m.coloca_barcosmanual(min_nave, m, tamano)
-    m.muestra_tablero(tablero,tamano)
+    min_nave = int(input("Cuantos barcos queire colocar; "))
+    Tablero = m.coloca_barcosmanual(min_nave, m, tamano)
+    m.muestra_tablero(Tablero,tamano)
+    borra = 0
+    while True:
+      if "X" in Tablero[0] or "X" in Tablero[1] or "X" in Tablero[2] or "X" in Tablero[3] or "X" in Tablero[4] or "X" in Tablero[5] or "X" in Tablero[6] or "X" in Tablero[7]:
+        Tablero = disparos(Tablero)
+        muestra_tablero(Tablero,tamano)
+        borra+=1
+      else: 
+        print("El juego ha terminado. Es hora de calcular la puntuación")
+        break
 else:
     print("Escoga uno de los modos")
     modojuego= (input("Bienvenido a Batalla Naval: n\ Escoja el modo de juego: Automático (A) o Manual (M): "))
