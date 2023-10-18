07e7529f2ed6607bfcc1d2738e1dbd87dbc721ef
paulinism <145799438+paulinism@users.noreply.github.com>
2023-10-18 15:45:16 -0600

    Update complete.py
    almost done

--- complete.py
+++ complete.py
@@ -28,6 +28,17 @@ def coloca_barcos(min_nave,m,tamano):
       barco_puesto += 1
   return m
 
+#funcion que coloca barcos manuales:
+def coloca_barcosmanual(min_nave,m,tamano):
+  barco_puesto = 0
+  while barco_puesto < min_nave:
+    fila = int(input("Inserte la fila del barco: "))
+    col = int(input("Inserte la columna del barco: "))
+    if m[fila-1][col-1] == 0:
+        m[fila-1][col-1] = "X"
+        barco_puesto += 1
+  return m
+
 #Muestra la matriz como tablero
 def muestra_tablero(tablero,tamano):
   print(" ",end = " ")
@@ -41,11 +52,7 @@ def muestra_tablero(tablero,tamano):
     print("")
 
 # Selecciona de los diferentes disparos con condiciones adicionales
-def disparos(tablero):
-  d = (input("Introduce el tipo de disparo: (o, *, -, +, \, /, X)"))
-  fila = int(input("Introduce la coordanada de la fila: "))-1
-  col = int(input("Introduce la coordanada de la columna: "))-1
-
+def disparos(tablero, d, fila, col):
   # Disparo simple (‘-’)
   if d=="-":
     tablero[fila][col]= "0"
@@ -130,49 +137,60 @@ def disparos(tablero):
     return tablero
 
 #Función para escoger aleatoriamente el disparo en modo automático
-def tiro_random(minimo_nave,m,tamaño, disparos):
-    disp_rand=0
-    while disp_rand<minimo_nave:
-        lista_disparos = ["o","*","-","+","\\","/","X"]
-        disp_aleatorio = rd.choice(lista_disparos)
-        print("Disparo actual: ", disp_aleatorio)
-        disp_rand+=1
-        disparos
-    return m,
+#def tiro_random(min_nave,m,tamaño):
+    #disp_rand=0
+    #while disp_rand<min_nave:
+        #lista_disparos = ["o","*","-","+","\\","/","X"]
+        #d = rd.choice(lista_disparos)
+        #print("Disparo actual: ", d) 
+        #disp_rand+=1
+        #fila = rd.randint(0, tamano-1)
+        #col = rd.randint(0,tamano-1) 
+    #return d, fila, col
+    
 
 #-------------------------Inicio del juego (menu)------------------------------
-def menu(min_nave, coloca_barcos, muestra_tablero, disparos, m, tamano, tiro_random):
+def menu(min_nave, coloca_barcos, muestra_tablero, disparos, m):
     modojuego=(input("Bienvenido a Batalla Naval: n\ Escoja el modo de juego: Automático (A) o Manual (M): "))
-
+    tamano = 8
     if modojuego in "Aa":
         print("Tendrás 35 oportunidades de ataque para eliminar todas las naves en la pantalla.")
         print("El número restante de ataques se multiplica por diez y se convierte en tu puntuación final")
         print("Por lo tanto, cuanto más ataques te queden, más alta será tu puntuación. ¡Aprovecha la oportunidad para eliminar las naves!")
-        min_nave = int(tamano*tamano*0.55/1)
-    
-        #Inicializar el tablero
         Tablero = coloca_barcos(min_nave, m, tamano)
         muestra_tablero(Tablero,tamano)
-    
+        
         # Para acumular disparos
         borra = 0
         while True:
             if "X" in Tablero[0] or "X" in Tablero[1] or "X" in Tablero[2] or "X" in Tablero[3] or "X" in Tablero[4] or "X" in Tablero[5] or "X" in Tablero[6] or "X" in Tablero[7]:
-                Tablero = disparos(Tablero)
-                muestra_tablero(Tablero,tamano)
-                borra+=1
+                disp_rand=0
+                while disp_rand<min_nave:
+                    lista_disparos = ["o","*","-","+","\\","/","X"]
+                    d = rd.choice(lista_disparos)
+                    print("Disparo actual: ", d) 
+                    disp_rand+=1
+                    fila = rd.randint(0, tamano-1)
+                    col = rd.randint(0,tamano-1) 
+                    Tablero = disparos(Tablero, d, fila, col)
+                    muestra_tablero(Tablero,tamano)
+                    borra+=1
             else: 
                 print("El juego ha terminado. Es hora de calcular la puntuación")
                 break
         
     elif modojuego in "Mm":
-        min_nave = int(tamano*tamano*0.55/1)
-        Tablero = coloca_barcos(min_nave, m, tamano)
+        min_nave = int(input("¿Cuántos barcos desea colocar?: "))
+        Tablero = coloca_barcosmanual(min_nave, m, tamano)
         muestra_tablero(Tablero,tamano)
+
         borra = 0
         while True:
             if "X" in Tablero[0] or "X" in Tablero[1] or "X" in Tablero[2] or "X" in Tablero[3] or "X" in Tablero[4] or "X" in Tablero[5] or "X" in Tablero[6] or "X" in Tablero[7]:
-                Tablero = disparos(Tablero)
+                d = (input("Introduce el tipo de disparo: (o, *, -, +, \, /, X)"))
+                fila = int(input("Introduce la coordanada de la fila: "))-1
+                col = int(input("Introduce la coordanada de la columna: "))-1
+                Tablero = disparos(Tablero, d, fila, col)
                 muestra_tablero(Tablero,tamano)
                 borra+=1
             else: 
@@ -187,4 +205,4 @@ def menu(min_nave, coloca_barcos, muestra_tablero, disparos, m, tamano, tiro_ran
 
 # Inicializar el juego  
 m = creaMatriz(tamano)
-menu(min_nave, coloca_barcos, muestra_tablero, disparos, m, tamano, tiro_random)
+menu(min_nave, coloca_barcos, muestra_tablero, disparos, m)
