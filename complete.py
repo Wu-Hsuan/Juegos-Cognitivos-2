d7b09f6b129491347b65c1638011e52933880b73
paulinism <145799438+paulinism@users.noreply.github.com>
2023-10-17 21:28:07 -0600

    Update complete.py

--- complete.py
+++ complete.py
@@ -43,14 +43,15 @@ def mostrar_tablero(tablero, tamaño):
         print(" ")
 
 # Selecciona de los diferentes disparos con condiciones adicionales
-def disparos(matriz, tamaño):
+def disparos(m, tamaño,):
     d= (input("Introduce el tipo de disparo: (o, *, -, +, \\, /, X)"))
     fila= int(input("Introduce la coordanada de la fila: "))
     col= int(input("Introduce la coordanada de la columna: "))
-  
+    
     # Disparo simple (‘-’)
     if d=="-":
         tablero[fila][col]== "0"
+        return tablero
     # Disparo en asterisco (‘*’)
     elif d=="*":
         tablero[fila][col]== "0"
@@ -62,6 +63,7 @@ def disparos(matriz, tamaño):
         tablero[fila-1][col+1]== "0"
         tablero[fila][col+1]== "0"
         tablero[fila+1][col+1]== "0"
+        return tablero
     # Disparo en cruz (‘+’)
     elif d=="+":
         tablero[fila][col]== "0"
@@ -69,23 +71,27 @@ def disparos(matriz, tamaño):
         tablero[fila+1][col]== "0"
         tablero[fila][col-1]== "0"
         tablero[fila][col+1]== "0"
+        return tablero
     # Disparo en equis (‘X’)
     elif d=="X":
         tablero[fila][col]== "0"
         tablero[fila-1][col-1]== "0"
         tablero[fila+1][col+1]== "0"
-        tablero[fila-1][col-10]== "0"
+        tablero[fila-1][col-1]== "0"
         tablero[fila-1][col+1]== "0"
+        return tablero
     # Disparo en diagonal (‘/’)
     elif d=="/":
         tablero[fila][col]== "0"
         tablero[fila-1][col+1]== "0"
         tablero[fila+1][col-1]== "0"
+        return tablero
     # Disparo en diagonal inversa (‘\’)
     elif d=="\\":
         tablero[fila][col] == "0"
         tablero[fila-1][col-1] == "0"
         tablero[fila+1][col+1] == "0"
+        return tablero
     # Disparo de dona (‘o’)
     elif d=="o":
       tablero[fila-1][col-1] == "0"
@@ -96,10 +102,12 @@ def disparos(matriz, tamaño):
       tablero[fila-1][col+1] == "0"
       tablero[fila][col+1] == "0"
       tablero[fila+1][col+1] == "0"
+      return tablero
     else:
         print("Disparo no existente")
     
-    print(tablero)
+    
+        
     
 #Función para escoger aleatoriamente el disparo en modo automático
 def tiro_random(minimo_nave,m,tamaño, disparos):
@@ -119,8 +127,8 @@ def menu(m, tablero, mostrar_tablero, disparos, minimo_nave,tiro_random):
     modojuego=(input("Bienvenido a Batalla Naval: \n Escoja el modo de juego: Automático (A) o Manual (M): "))
     if modojuego in "Aa":
         print("Se jugará solo en juego")
-        tablero = disparos(tablero)
         mostrar_tablero(tablero,tamaño)
+        disparos(tablero, tamaño)
         tiro_random(minimo_nave, m, tamaño)
         # Para acumular disparos
         borra = 0
@@ -134,9 +142,8 @@ def menu(m, tablero, mostrar_tablero, disparos, minimo_nave,tiro_random):
         print("Tendrás 35 oportunidades de ataque para eliminar todas las naves en la pantalla.")
         print("El número restante de ataques se multiplica por diez y se convierte en tu puntuación final")
         print("Por lo tanto, cuanto más ataques te queden, más alta será tu puntuación. ¡Aprovecha la oportunidad para eliminar las naves!")
-        mostrar_tablero(tablero, tamaño) 
-        disparos(m, tamaño)
-    
+        mostrar_tablero(tablero,tamaño)
+        disparos(tablero, tamaño)
         borra = 0
         while True:
             if "X" in tablero[0] or "X" in tablero[1] or "X" in tablero[2] or "X" in tablero[3] or "X" in tablero[4] or "X" in tablero[5] or "X" in tablero[6] or "X" in tablero[7]:
